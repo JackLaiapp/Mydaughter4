95c604bfa8ddeed3dc98f2b0d23d0f630714eecb
MyMac <mymac@mymac-no-macbook-pro.local>
2017-03-22 23:05:48 +0800

    加註解

--- Mydaughter/MydaughterViewController.swift
+++ Mydaughter/MydaughterViewController.swift
@@ -65,23 +65,27 @@ class MydaughterViewController: UIViewController, UITableViewDataSource, UITable
     }
     
     func choosePicture(){
+        // 設置拍照完後 可以編輯 會多一個編輯照片的步驟
         imagePicker.allowsEditing = false
+        // 設定影像來源 這裡設定為圖庫
         imagePicker.sourceType = .photoLibrary
-        
+        // 顯示圖庫功能
         present(imagePicker, animated: true, completion: nil)
     }
 
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
-
+        // 取得原始拍照的照片 UIImage
         let picker:UIImage = info[UIImagePickerControllerOriginalImage] as! UIImage
         
+         // 再來就是對圖片的處理 img 是一個 UIImage
         //button.setImage(picker, for: .normal)
-
+        //移除Picker
         dismiss(animated: true, completion: nil)
     }
     
     override func viewDidLoad() {
     super.viewDidLoad()
+        // 設置 delegate
         imagePicker.delegate = self
         
     // Do any additional setup after loading the view.
